e00f4f308feafa4e2c4316012eba62856c4c29f2
Dave Fisher <dave.j.fish@gmail.com>
2022-08-02 12:04:48 -0700

    test written for adding author

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -8,20 +8,37 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
 
-  it('should return a list of authors', async () => {
+  it('#GET should return a list of authors', async () => {
     const response = await request(app).get('/authors');
     expect(response.body.length).toEqual(4);
   });
 
-  it('should return an author with a detail of books written', async () => {
+  it('#GET should return an author with a detail of books written', async () => {
     const response = await request(app).get('/authors/1');
     const bob = response.body;
     expect(bob).toHaveProperty('name', 'bob');
-    expect(bob).toHaveProperty('dob', '1900-05-05');
+    expect(bob).toHaveProperty('dob', '1900-05-05T08:00:00.000Z');
     expect(bob).toHaveProperty('pob', 'over there');
     expect(bob).toHaveProperty('books');
     expect(bob.books[0]).toHaveProperty('title');
     expect(bob.books[0]).toHaveProperty('released');
+  });
+
+  it('#POST should add a new author', async () => {
+    const response = await request(app)
+      .post('/authors')
+      .send({
+        name: 'author man',
+        dob: '1850-01-01',
+        pob: 'india'
+      });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      dob: expect.any(String),
+      pob: expect.any(String),
+    });
   });
 
   afterAll(() => {
